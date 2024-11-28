cfb1a2c3a69a4b2a0a1d903dff64c390cbd8efe6
abdelmoudiri <abdeljabbarmoudiri17@gmail.com>
2024-11-28 12:06:58 +0100

    ajouter les joueurs au changments

--- asset/js/script.js
+++ asset/js/script.js
@@ -27,6 +27,11 @@ let Allplayers = [];
 let temData=[];
 let playerDiv; 
 
+btnAjouter.addEventListener("click",()=>{
+  toggleVisibility(crud_modal);
+  toggleVisibility(Div_add_player);
+})
+
 function toggleVisibility(element) {
   if (element.classList.contains('hidden')) {
       element.classList.remove('hidden');
@@ -278,19 +283,71 @@ document.getElementById('ajouterAlocalStorage').addEventListener('click', addPla
       if (player.id == id) {  
         switch (player.position) {
           case 'LW':
-              div_LW.appendChild(divCard);
+              if (div_LW.innerHTML.trim() === '') {
+                div_LW.appendChild(divCard);
+              } else {
+                AjouterAu_Changement(divCard)
+              }
               break;
+
+            
           case 'ST':
-              div_ST.appendChild(divCard);
-              break;
+                if (div_ST.innerHTML.trim() === '') {
+                  div_ST.appendChild(divCard);
+                } else {
+                  AjouterAu_Changement(divCard)
+                }   
+                break;
+
           case 'RW':
+            if (div_RW.innerHTML.trim() === '') {
               div_RW.appendChild(divCard);
+            } else {
+              AjouterAu_Changement(divCard)
+          }
               break;
           case 'CM':
+            if (div_CM.innerHTML.trim() === '') {
               div_CM.appendChild(divCard);
+            } else {
+              AjouterAu_Changement(divCard)
+          }
               break;
+          case 'LB':
+            if (div_LB.innerHTML.trim() === '') {
+              div_LB.appendChild(divCard);
+            } else {
+              AjouterAu_Changement(divCard)
+          }
+              break;
+          case 'CB':
+            if (div_CB.innerHTML.trim() === '') {
+              div_CB.appendChild(divCard);
+            } else {
+              AjouterAu_Changement(divCard)
+          }
+              
+              break;
+          case 'CB':
+                  if (div_CB1.innerHTML.trim() === '') {
+                    div_CB1.appendChild(divCard);
+                  } else {
+                    AjouterAu_Changement(divCard)
+                  }
+              break;
+          case 'RB':
+                if (div_RB.innerHTML.trim() === '') {
+                div_RB.appendChild(divCard);
+                }else {
+                  AjouterAu_Changement(divCard)
+                }
+                break;
           case 'GK':
+            if (div_GK.innerHTML.trim() === '') {
               div_GK.appendChild(divCard);
+            } else {
+              AjouterAu_Changement(divCard)
+            }
               break;
           default:
               console.log('Unknown position');
@@ -300,4 +357,76 @@ document.getElementById('ajouterAlocalStorage').addEventListener('click', addPla
     });
   }
   
+  function AjouterAu_Changement(joueur)
+  {
+    playerDiv = document.createElement('div');
+    playerDiv.setAttribute('draggable', 'true');
+    playerDiv.setAttribute('id', `${joueur.id}`);  
+    playerDiv.setAttribute('onclick',"funcAjouter_Terrain(this,this.id);");  
+    playerDiv.className = "relative flex items-center justify-center";
+    playerDiv.innerHTML = `
+      <div class="relative w-[100px] h-[180px] bg-cover bg-center bg-[url('https://selimdoyranli.com/cdn/fut-player-card/img/card_bg.png')] transition-all ease-in">
+        <div class="relative flex text-[#e9cc74] px-[0.3rem]">
+          <div class="absolute py-[0.8rem_0] text-xs uppercase font-light">
+            <div class="text-[1rem] mt-5">${joueur.rating}</div>
+            <div>${joueur.position}</div>
+            <div class="block">
+              <img src="${joueur.flag}" alt="${joueur.nationality}" class="w-[1rem] h-[14px] object-contain" />
+            </div>
+            <div class="block">
+              <img src="${joueur.logo}" alt="${joueur.club}" class="w-[1rem] h-[14px] object-contain" />
+            </div>
+          </div>
+          <div class="w-[70px] h-[80px] mx-auto overflow-hidden">
+            <img src="${joueur.photo}" alt="${joueur.name}" class="w-full h-full object-contain relative right-[-1rem] bottom-0" />
+          </div>
+        </div>
+
+        <div class="w-full flex justify-around text-[#88e635] text-[0.7rem] font-bold uppercase">
+          <span class="ml-[0.4rem] text-shadow-lg">${joueur.position}</span>
+        </div>
+
+        <div class="relative">
+          <div class="text-[#e9cc74] w-[90%] mx-auto">
+            <div class="text-center w-[100%] text-[0.6rem] uppercase border-b-2 border-[#e9cc74]/[0.1] ">
+              <span class="block text-shadow-lg">${joueur.name}</span>
+            </div>
+            <div class="flex justify-center ">
+              <div class="pr-[1.5rem] border-r-2 border-[#e9cc74]/[0.1]">
+                <div class="flex items-center text-[0.5rem] uppercase">
+                  <span class="font-bold mr-[0.3rem]">${joueur.pace}</span>
+                  <span class="font-light">PAC</span>
+                </div>
+                <div class="flex items-center text-[0.5rem] uppercase">
+                  <span class="font-bold mr-[0.3rem]">${joueur.shooting}</span>
+                  <span class="font-light">SHO</span>
+                </div>
+                <div class="flex items-center text-[0.5rem] uppercase">
+                  <span class="font-bold mr-[0.3rem]">${joueur.passing}</span>
+                  <span class="font-light">PAS</span>
+                </div>
+              </div>
+              <div>
+                <div class="flex items-center text-[0.5rem] uppercase">
+                  <span class="font-bold mr-[0.3rem]">${joueur.dribbling}</span>
+                  <span class="font-light">DRI</span>
+                </div>
+                <div class="flex items-center text-[0.5rem] uppercase">
+                  <span class="font-bold mr-[0.3rem]">${joueur.defending}</span>
+                  <span class="font-light">DEF</span>
+                </div>
+                <div class="flex items-center text-[0.5rem] uppercase">
+                  <span class="font-bold mr-[0.3rem]">${joueur.physical}</span>
+                  <span class="font-light">PHY</span>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    `;
+    document.getElementById('Bonne_touche').appendChild(playerDiv);
+    
+  }
+
   
